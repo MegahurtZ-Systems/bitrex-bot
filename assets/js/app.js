7dc6054e3a1ab16a24e887d427078045b71186e8
Jayms <jayms.portune@gmail.com>
2017-12-01 23:55:32 -0800

    adding in the capability to import a csv file to an array, still dealing with an error in the console for the allTestLines.split line in the reader.onload

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
-'use strict';
-
 /* Set the width of the side navigation to 250px */
+var fileInput = document.getElementById('csv');
+var fileData = [];
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
@@ -9,3 +10,23 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+readFile = function(csv){
+  var reader = new FileReader();
+  reader.onload = function(){
+    var csv = event.target.result;
+    var allTextLines = csv.split(/\r\n|\n/);
+    for (var i = 0; allTextLines.length; i++){
+      var data = allTextLines[i].split(';');
+      var tarr = [];
+      for (var j = 0; j < data.length; j++){
+        tarr.push(data[j]);
+      }
+      fileData.push(tarr);
+    }
+  console.log(fileData);
+  // document.getElementById('output').innerHTML = reader.result;
+  };
+  reader.readAsBinaryString(fileInput.files[0]);
+};
+fileInput.addEventListener('change', readFile);
