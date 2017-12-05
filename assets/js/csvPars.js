3fcdc2e4d338a111763ba6b1249e0a44d4c56fd4
dambergn <dambergn@gmail.com>
2017-12-04 21:16:32 -0800

    fixed side nav and seperated app.js into main.js and csvPars.js for organization.

--- assets/js/csvPars.js
+++ assets/js/csvPars.js
@@ -2,18 +2,10 @@
 var fileInput = document.getElementById('csv');
 var fileData = [];
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
-
-/* Set the width of the side navigation to 0 */
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
 readFile = function(csv){
   var reader = new FileReader();
   reader.onload = function(){
+    //debugger;
     var csv = event.target.result;
     var allTextLines = csv.split(/\r\n|\n/);
     for (var i = 0; allTextLines.length; i++){
