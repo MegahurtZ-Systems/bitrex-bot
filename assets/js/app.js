0b882b662b4be07e5f3e4ed9875b6bc4c8e40f4d
dambergn <dambergn@gmail.com>
2017-12-21 01:15:31 -0800

    temp

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,3 +9,15 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+//document.write('test');
+
+const url.no-cors = 'https://bittrex.com/api/v1.1/public/getmarkets';
+fetch(url) // Call the fetch function passing the url of the API as a parameter
+.then(function() {
+  console.log('test');
+    // Your code for handling the data you get from the API
+})
+.catch(function() {
+    // This is where you run code if the server returns any errors
+});
