4ad9453645723185f5d7708e221bf8cb0cc86523
dambergn <dambergn@gmail.com>
2017-12-31 18:32:47 -0800

    added bittrex-node package.

--- server.js
+++ server.js
@@ -2,15 +2,20 @@
 /*To run the server install the latest version of nodejs and exicute this server.js file
 from the termial using nodejs.  Example: nodejs server.js */
 
+//const p5 = require('p5');
 const pg = require('pg');//for connection to PostgreSQL database.
 const fs = require('fs');//for communication with File System.
 const express = require('express');//web server.
 const bodyParser = require('body-parser');
+const bittrex = require('node-bittrex-api');
 const PORT = process.env.PORT || 3005;//listen port for server.
 const app = express();//runs the web server application.
 
-const conString = 'postgres://postgres:GiGahurtZ42@localhost:5432/kilovolt2';//credentials psql database.
+const conString = 'postgres://postgres:GiGahurtZ42@localhost:5432/data';//credentials psql database.
 const client = new pg.Client(conString);//uses credentials to connecto to psql database.
+const API_KEY = '';
+const API_SECRET ='';
+
 client.connect();
 client.on('error', error => {
   console.error(error);
@@ -25,6 +30,35 @@ app.get('/new', (request, response) => {
   response.sendFile('new.html', {root: './public'});
 });
 
+bittrex.options({
+  'apikey' : API_KEY,
+  'apisecret' : API_SECRET,
+});
+
+
+
 app.listen(PORT, () => {
   console.log(`Server started on port: ${PORT}!`);
+});
+
+
+bittrex.getmarketsummaries( function( data, err ) {
+  if (err) {
+    return console.error(err);
+  }
+  for( var i in data.result ) {
+    bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
+      //console.log( ticker );
+    });
+  }
+});
+
+bittrex.getmarkethistory( { market : 'BTC-ETH' }, function( data ) {
+  console.log( data.result );
+});
+
+bittrex.getorderbook( { market : 'BTC-PIVX', depth : 10, type : 'both' }, function( data ) {
+
+    //data.result.buy.forEach(function(dataset) { console.log(dataset); });
+    //data.result.sell.forEach(function(dataset) { console.log(dataset); });
 });
